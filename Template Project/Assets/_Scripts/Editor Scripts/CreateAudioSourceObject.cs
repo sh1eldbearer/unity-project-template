da9361dcd6e4aaae6b480848a1470c32b898f83c
Nathan Scott <natscott@uat.edu>
2020-05-08 09:32:29 -0700

    Reclassified menu scripts as editor scripts

---
+++ Template Project/Assets/_Scripts/Editor Scripts/CreateAudioSourceObject.cs
@@ -0,0 +1,26 @@
+﻿#if UNITY_EDITOR
+
+// This script is only used in the UnityEditor, so the directive tells the compiler to ignore it.
+    using DataClasses;
+    using UnityEditor;
+    using UnityEngine;
+
+    namespace EditorScripts
+    {
+        public class CreateAudioSourceObject : MonoBehaviour
+        {
+            [MenuItem("GameObject/Audio/Custom Audio Source Object", false, 10)]
+            static void CreateAudioSourceGameObject(MenuCommand menuCommand)
+            {
+                // Create a new GameObject
+                GameObject newGameObject = new GameObject("New Audio Source Object", typeof(AudioSourceInfo));
+                // Ensure it gets re-parented if this was a context click (otherwise does nothing)
+                GameObjectUtility.SetParentAndAlign(newGameObject, menuCommand.context as GameObject);
+                // Register the creation in the undo system
+                Undo.RegisterCreatedObjectUndo(newGameObject, "Create " + newGameObject.name);
+                // Select the new GameObject in the hierarchy
+                Selection.activeObject = newGameObject;
+            }
+        }
+    }
+#endif
