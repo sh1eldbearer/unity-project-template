a490cb125b1892af4fd9f1eb8feadb413a452437
Nathan Scott <natscott@uat.edu>
2020-05-12 18:26:18 -0700

    Added overload to and streamlined Singleton class

--- Template Project/Assets/_Scripts/Utilities/Singleton.cs
+++ Template Project/Assets/_Scripts/Utilities/Singleton.cs
@@ -14,41 +14,6 @@ namespace Utilities
         /// <summary>
         /// Attempts to create a singleton instance of the class of the given Type.
         ///
-        /// Will also attempt to flag the provided GameObject as DontDestroyOnLoad, if the
-        /// GameObject is not a child of any other GameObject.
-        ///
-        /// If the class of the provided Type already has a defined static instance, the GameObject
-        /// (and the script stored on it) will be considered redundant, and destroyed.
-        /// </summary>
-        /// <typeparam name="Type">The class you are attempting to set a singleton instance of.</typeparam>
-        /// <param name="script">The script object invoking the singleton attempt.</param>
-        /// <param name="instance">The Type class' static instance variable.</param>
-        /// <param name="gameObject">The GameObject on which the calling script resides.</param>
-        public static void SetAsStaticInstance(Type script, ref Type instance, GameObject gameObject)
-        {
-            // Checks to see if this class already has a defined static instance
-            if (instance == null)
-            {
-                // Assign the script as the static instance
-                instance = script;
-
-                // Checks to see if the GameObject is a child
-                if (gameObject.transform.parent == null)
-                {
-                    // If this GameObject is not a child, set it as a persistent object
-                    MonoBehaviour.DontDestroyOnLoad(gameObject);
-                }
-            }
-            else
-            {
-                // The class already has a defined static instance, so destroy the GameObject (and the script)
-                MonoBehaviour.Destroy(gameObject);
-            }
-        }
-
-        /// <summary>
-        /// Attempts to create a singleton instance of the class of the given Type.
-        ///
         /// If the class of the provided Type already has a defined static instance, the script will be
         /// considered redundant, and destroyed.
         /// </summary>
@@ -57,16 +22,77 @@ namespace Utilities
         /// <param name="instance">The Type class' static instance variable.</param>
         public static void SetAsStaticInstance(Type script, ref Type instance)
         {
+            SetAsStaticInstance(script, ref instance, null, false);
+        }
+
+        /// <summary>
+        /// Attempts to create a singleton instance of the class of the given Type.
+        /// 
+        /// Will also attempt to flag the provided GameObject as DontDestroyOnLoad, if the
+        /// GameObject is not a child of any other GameObject.
+        /// 
+        /// If the class of the provided Type already has a defined static instance, the GameObject
+        /// (and the script stored on it) will be considered redundant, and destroyed.
+        /// </summary>
+        /// <typeparam name="Type">The class you are attempting to set a singleton instance of.</typeparam>
+        /// <param name="script">The script object invoking the singleton attempt.</param>
+        /// <param name="instance">The Type class' static instance variable.</param>
+        /// <param name="gameObject">The GameObject on which the calling script resides.</param>
+        /// </param>
+        public static void SetAsStaticInstance(Type script, ref Type instance, GameObject gameObject)
+        {
+            SetAsStaticInstance(script, ref instance, gameObject, false);
+        }
+
+        /// <summary>
+        /// Attempts to create a singleton instance of the class of the given Type.
+        /// 
+        /// Will also attempt to flag the provided GameObject as DontDestroyOnLoad, if the
+        /// GameObject is not a child of any other GameObject.
+        /// 
+        /// If the class of the provided Type already has a defined static instance, the GameObject
+        /// (and the script stored on it) will be considered redundant, and destroyed.
+        /// </summary>
+        /// <typeparam name="Type">The class you are attempting to set a singleton instance of.</typeparam>
+        /// <param name="script">The script object invoking the singleton attempt.</param>
+        /// <param name="instance">The Type class' static instance variable.</param>
+        /// <param name="gameObject">The GameObject on which the calling script resides.</param>
+        /// <param name="destroyOtherInstance">Denotes whether or not an already-existing static instance of
+        /// a class should be deleted and replaced by a new instance of the class.</param>
+        public static void SetAsStaticInstance(Type script, ref Type instance, GameObject gameObject, 
+            bool destroyOtherInstance)
+        {
+            if (destroyOtherInstance && instance != null)
+            {
+                MonoBehaviour.Destroy((instance as MonoBehaviour).gameObject);
+            }
+
             // Checks to see if this class already has a defined static instance
             if (instance == null)
             {
                 // Assign the script as the static instance
                 instance = script;
+
+                // Checks to see if the GameObject is a child
+                if (gameObject != null && gameObject.transform.parent == null)
+                {
+                    // If this GameObject is not a child, set it as a persistent object
+                    MonoBehaviour.DontDestroyOnLoad(gameObject);
+                }
             }
             else
             {
-                // The class already has a defined static instance, so destroy the script
-                MonoBehaviour.Destroy(script as Object);
+                if (gameObject == null)
+                {
+                    // The class already has a defined static instance, so destroy the script
+                    MonoBehaviour.Destroy(script as Object);
+                }
+                else
+                {
+
+                    // The class already has a defined static instance, so destroy the GameObject (and the script)
+                    MonoBehaviour.Destroy(gameObject);
+                }
             }
         }
     }
