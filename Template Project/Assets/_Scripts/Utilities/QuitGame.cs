356c6c95fe500559b02c94a8d8891e033fdfe1cf
Nathan Scott <natscott@uat.edu>
2020-05-12 18:26:34 -0700

    Added QuitGame script

---
+++ Template Project/Assets/_Scripts/Utilities/QuitGame.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Utilities
+{
+    public class QuitGame : MonoBehaviour
+    {
+        /// <summary>
+        /// Quits the game.
+        /// </summary>
+        public void Quit()
+        {
+            #if UNITY_EDITOR
+                EditorApplication.isPlaying = false;
+            #else
+                Application.Quit();
+            #endif
+        }
+    }
+}
