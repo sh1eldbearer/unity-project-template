d69705b2df9ab3db239b663a7a4602da138fbb07
Nathan Scott <natscott@uat.edu>
2020-05-14 17:24:02 -0700

    Testing for InputManager; will come back to this later

---
+++ Template Project/Assets/_Scripts/Data Classes/InputInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataClasses
+{
+    [System.Serializable]
+    public class InputInfo
+    {
+        #region Private Properties
+
+#pragma warning disable CS0649
+        [Tooltip("The name used for this input."),
+            SerializeField] private string _name;
+        [Tooltip("The KeyCode used by this action."),
+            SerializeField] private KeyCode _keyCode;
+#pragma warning restore CS0649
+
+        #endregion
+
+        #region Public Properties
+
+        #endregion
+    }
+}
---
+++ Template Project/Assets/_Scripts/Manager Objects/InputManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InputManager : MonoBehaviour
+{
+    #region Private Properties
+#pragma warning disable CS0649
+    [SerializeField] public List<UnityEvent> _events;
+#pragma warning restore CS0649
+    #endregion
+
+    #region Public Properties
+
+    #endregion
+	
+	// Awake is called before Start
+	private void Awake()
+	{
+		
+	}
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        
+    }
+}
